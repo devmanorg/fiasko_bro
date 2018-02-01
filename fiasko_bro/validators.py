ca05768473c8e72a89ced8f025ce26c11501a7b0
Paul Vergeev <paulvergeev@gmail.com>
2018-02-01 15:32:42 +0300

    Move filename extraction into separate method

--- fiasko_bro/validators.py
+++ fiasko_bro/validators.py
@@ -449,5 +449,5 @@ def has_no_long_files(solution_repo, max_number_of_lines, *args, **kwargs):
     for file_path, file_content, _ in solution_repo.get_ast_trees(with_filenames=True, with_file_content=True):
         number_of_lines = file_content.count('\n')
         if number_of_lines > max_number_of_lines:
-            file_name = file_path[file_path.rfind('/') + 1:]
+            file_name = url_helpers.get_filename_from_path(file_path)
             return 'file_too_long', file_name
