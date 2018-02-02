ebd1a2dc6958dfbcb4b5e13ecb6afb5fe6736748
vladimir.aleshin <vladimir.aleshin@osinit.com>
2018-02-02 16:58:38 +0300

    Fix for code style.

--- tests/test_general_validators/test_mccabe_difficulty.py
+++ tests/test_general_validators/test_mccabe_difficulty.py
@@ -4,5 +4,8 @@ from fiasko_bro import validators
 def test_mccabe_difficulty(test_repo):
     max_complexity = 7
     expected_output = 'mccabe_failure', 'function_for_test_mccabe_fail'
-    output = validators.is_mccabe_difficulty_ok(test_repo, max_complexity)
+    output = validators.is_mccabe_difficulty_ok(
+        solution_repo=test_repo,
+        max_complexity=max_complexity
+    )
     assert output == expected_output
