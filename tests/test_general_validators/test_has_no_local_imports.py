649e97dc9aacef3cd8a0c5c7028b8878d31e983c
Ranc58 <rancvova@gmail.com>
2018-02-03 13:04:12 +0300

    issue #6: Add tests for local and star imports

---
+++ tests/test_general_validators/test_has_no_local_imports.py
@@ -0,0 +1,21 @@
+from fiasko_bro import validators
+from fiasko_bro.code_validator import CodeValidator
+
+
+def test_no_local_imports_fail(test_repo):
+    expected_output = 'has_local_import', ''
+    whitelists = CodeValidator.whitelists
+    output = validators.has_no_local_imports(
+        solution_repo=test_repo,
+        whitelists=whitelists,
+    )
+    assert output == expected_output
+
+
+def test_no_local_imports_ok(test_repo):
+    whitelists = {'has_no_local_imports': ['no_local_imports_test_file.py']}
+    output = validators.has_no_local_imports(
+        solution_repo=test_repo,
+        whitelists=whitelists,
+    )
+    assert output is None
