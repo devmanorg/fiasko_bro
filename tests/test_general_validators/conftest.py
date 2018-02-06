48f2c7d39320bd859a0eb186251bb17a364ade71
Ranc58 <rancvova@gmail.com>
2018-02-06 20:52:00 +0300

    Fix mccabe test for new func name. Add edited conftest without session fixture.

--- tests/test_general_validators/conftest.py
+++ tests/test_general_validators/conftest.py
@@ -17,10 +17,3 @@ def origin_repo():
     origin_repo_dir = 'test_fixtures/general_repo_origin'
     git.Repo.init(origin_repo_dir)
     return LocalRepositoryInfo(origin_repo_dir)
-
-
-@pytest.fixture(scope='session')
-def temp_dir_for_tests(tmpdir_factory):
-    temp_repo_dir = tmpdir_factory.mktemp('temp_dir_for_test')
-    git.Repo.init(temp_repo_dir)
-    return temp_repo_dir
