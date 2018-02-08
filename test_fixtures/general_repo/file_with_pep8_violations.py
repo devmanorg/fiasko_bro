630a5ea34863e529c0a53f5efa2199ab99695d1e
Artem Chernyaev <askanio234@gmail.com>
2018-02-08 17:49:22 +0300

    Add test fixture for pep8 violations

---
+++ test_fixtures/general_repo/file_with_pep8_violations.py
@@ -0,0 +1,17 @@
+def very_long_function_name(argument, other):
+    return argument>other
+
+def multiply_if_conditonal(variable,other_variable,                         third_variable):
+    ham                             =[1,2,3]
+    variable+=ham[ 1 ]
+    if third_variable:
+        some_variable=variable*other_variable
+    else:
+        some_variable=other_variable/variable
+
+    variable = ((other_variable +
+    third_variable)
+    *third_variable)
+    return very_long_function_name (variable, 
+                                            some_variable,
+    third_variable)
