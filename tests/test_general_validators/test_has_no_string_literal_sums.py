36a0a75a4a9be96b388851942c77dd2abe31d751
mcproger <mcproger@iMini.local>
2018-02-09 23:59:04 +0300

    Replace test_has_no_string_sums test and file title with test_has_no_string_literal_sums

---
+++ tests/test_general_validators/test_has_no_string_literal_sums.py
@@ -0,0 +1,7 @@
+from fiasko_bro import validators
+
+
+def test_has_no_string_literal_sums_fail(test_repo):
+    expected_output = 'has_string_sum', ''
+    output = validators.has_no_string_literal_sums(solution_repo=test_repo)
+    assert output == expected_output
