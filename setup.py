2f69b8f8c58a997a19a5d209541f189b274dc121
Paul Vergeev <paulvergeev@gmail.com>
2018-02-10 22:03:43 +0300

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -25,23 +25,18 @@ class InstallWithCompile(install):
 setup(
     name='Fiasko Bro',
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
     version='0.0.1',
 
     description='Automatic code validator',
     long_description='The project validates for common pitfalls',  # TODO: generate README
 
-    # TODO: The project's main homepage.
-    # url='https://github.com/whatever/whatever',
+    url='https://github.com/devmanorg/fiasko_bro',
+
+    license='MIT',
 
     # TODO: Author details
     # author='yourname',
     # author_email='your@address.com',
-
-    # TODO: Choose your license
-    # license='MIT',
 
     # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
     # TODO: Classifiers
@@ -64,7 +59,7 @@ setup(
     #     'Programming Language :: Python :: 2.7',
     # ],
 
-    # What does your project relate to?
+    # TODO: Keywords
     # keywords='sample setuptools development',
 
     packages=['fiasko_bro'],
