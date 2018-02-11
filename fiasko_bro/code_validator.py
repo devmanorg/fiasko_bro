b06c0546c27c1ac1b1a8c5c6c46151217236943d
melevir <melevir@gmail.com>
2018-02-11 21:04:17 +0300

    Introduce config.py

--- fiasko_bro/code_validator.py
+++ fiasko_bro/code_validator.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 from . import validators
 from .repository_info import LocalRepositoryInfo
 from .code_helpers import is_repo_too_large
+from . import config
 
 
 def validate_repo(path_to_repo, path_to_original_repo=None):
@@ -11,140 +12,11 @@ def validate_repo(path_to_repo, path_to_original_repo=None):
 
 
 class CodeValidator:
-    blacklists = {
-        'has_variables_from_blacklist': [
-            'list',
-            'lists',
-            'input',
-            'cnt',
-            'data',
-            'name',
-            'load',
-            'value',
-            'object',
-            'file',
-            'result',
-            'item',
-            'num',
-            'info',
-            'n',
-        ],
-        'has_no_commit_messages_from_blacklist': [
-            'win',
-            'commit',
-            'commit#1',
-            'fix',
-            'minor edits',
-            'update',
-            'done',
-            'first commit',
-            'start',
-            'refactor',
-            '!',
-            'bug fix',
-            'corrected',
-            'add files via upload',
-            'test',
-            'fixed',
-            'minor bugfix',
-            'minor bugfixes',
-            'finished',
-            'first commit',
-            'fixes',
-            '',
-        ],
-        'has_no_directories_from_blacklist': [
-            '.idea',
-            '__pycache__',
-            '.vscode',
-        ],
-    }
+    blacklists = config.DEFAULT_BLACKLISTS
 
-    whitelists = {
-        'has_no_short_variable_names': [
-            'a',
-            'b',
-            'c',
-            'x',
-            'y',
-            'x1',
-            'x2',
-            'y1',
-            'y2',
-            '_',
-        ],
-        'has_no_calls_with_constants': [
-            'pow',
-            'exit',
-            'round',
-            'range',
-            'enumerate',
-            'time',
-            'itemgetter',
-            'get',
-            'group',
-            'replace',
-            'combinations',
-            'seek',
-        ],
-        'is_snake_case': [
-            # from sqlalchemy.sqlalchemy.orm.sessionmaker
-            'Session',
-            # from sqlalchemy.ext.automap
-            'Base',
-            'User',
-            'Order',
-            'Address',
-        ],
-        'right_assignment_for_snake_case': [
-            'Base',
-        ],
-        'has_no_exit_calls_in_functions': [
-            'main',
-        ],
-        'is_pep8_fine': [
-            '/migrations/',
-            '/alembic/',
-            'manage.py',
-        ],
-        'has_no_encoding_declaration': [
-            '/migrations/',
-        ],
-        'has_no_local_imports': [
-            'manage.py',
-        ],
-        'has_local_var_named_as_global': [
-            'settings.py',
-        ],
-        'has_variables_from_blacklist': [
-            'apps.py',
-        ],
-        'has_no_extra_dockstrings_whitelist': [
-            '/migrations/',
-            '/alembic/',
-        ],
-        'is_nesting_too_deep': [
-            '/migrations/',
-            '/alembic/',
-            'manage.py',
-            'settings.py',
-        ],
-    }
+    whitelists = config.DEFAULT_WHITELISTS
 
-    _default_settings = {
-        'readme_filename': 'README.md',
-        'allowed_max_pep8_violations': 5,
-        'max_complexity': 7,
-        'minimum_name_length': 2,
-        'min_percent_of_another_language': 30,
-        'last_commits_to_check_amount': 5,
-        'tab_size': 4,
-        'functions_with_docstrings_percent_limit': 80,
-        'max_pep8_line_length': 100,
-        'max_number_of_lines': 200,
-        'max_indentation_level': 4,
-        'max_num_of_py_files': 100,
-    }
+    _default_settings = config.VALIDATOR_SETTINGS
 
     error_validator_groups = OrderedDict(
         [
