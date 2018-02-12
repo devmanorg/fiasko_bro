0afa44728f0434b5f4618fb77146c2180e992dfb
Paul Vergeev <paulvergeev@gmail.com>
2018-02-12 22:31:22 +0300

    Revert "Fix git add files for Travis"
    This reverts commit a20aa568a94446301990108edbc3fbab58a5b070.

--- tests/test_commits_validators/conftest.py
+++ tests/test_commits_validators/conftest.py
@@ -1,6 +1,5 @@
-import os
-
 import pytest
+
 import git
 
 from fiasko_bro.repository_info import LocalRepositoryInfo
@@ -8,21 +7,19 @@ from fiasko_bro.repository_info import LocalRepositoryInfo
 
 @pytest.fixture(scope="module")
 def test_repo():
-    test_repo_dir = 'test_fixtures/commits_repo/'
+    test_repo_dir = 'test_fixtures/commits_repo'
     repo = git.Repo.init(test_repo_dir)
-    fixture_path = os.path.abspath(test_repo_dir)
-    repo.index.add(['{}{}{}'.format(fixture_path, os.path.sep, 'initial_file.py')])
+    repo.index.add(['initial_file.py'])
     repo.index.commit('Initial commit')
-    repo.index.add(['{}{}{}'.format(fixture_path, os.path.sep, 'second_commit_file.py')])
+    repo.index.add(['second_commit_file.py'])
     repo.index.commit('win')
     return LocalRepositoryInfo(test_repo_dir)
 
 
 @pytest.fixture(scope="module")
 def origin_repo():
-    origin_repo_dir = 'test_fixtures/commits_repo_origin/'
+    origin_repo_dir = 'test_fixtures/commits_repo_origin'
     repo = git.Repo.init(origin_repo_dir)
-    fixture_path = os.path.abspath(origin_repo_dir)
-    repo.index.add(['{}{}{}'.format(fixture_path, os.path.sep, 'initial_file.py')])
+    repo.index.add(['initial_file.py'])
     repo.index.commit('Initial commit')
     return LocalRepositoryInfo(origin_repo_dir)
