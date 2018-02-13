ab9e6daba831c823676ed969425373ce60b0d3b5
Paul Vergeev <paulvergeev@gmail.com>
2018-02-13 21:29:45 +0300

    Add tests for has_no_slices_starts_from_zero validator

---
+++ tests/test_general_validators/test_has_no_slices_starts_from_zero.py
@@ -0,0 +1,13 @@
+from fiasko_bro.validators import has_no_slices_starts_from_zero
+
+
+def test_has_no_slices_starts_from_zero_fails(test_repo):
+    expected_output = 'slice_starts_from_zero', ''
+    output = has_no_slices_starts_from_zero(test_repo)
+    assert output == expected_output
+
+
+def test_has_no_slices_starts_from_zero_succeeds(origin_repo):
+    output = has_no_slices_starts_from_zero(origin_repo)
+    assert output is None
+
