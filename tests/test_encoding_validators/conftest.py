d266ef7470ea515c6b60eac0def8db7e6855343d
Artem Chernyaev <askanio234@gmail.com>
2018-02-17 14:34:00 +0300

    Rename fixtures for encoding tests

--- tests/test_encoding_validators/conftest.py
+++ tests/test_encoding_validators/conftest.py
@@ -4,12 +4,12 @@ import pytest
 
 
 @pytest.fixture(scope="module")
-def encoding_repo():
+def encoding_repo_path():
     encoding_repo_dir = 'test_fixtures{}encoding_repo'.format(os.path.sep)
     return encoding_repo_dir
 
 
 @pytest.fixture(scope="module")
-def general_repo():
+def general_repo_path():
     general_repo_dir = 'test_fixtures{}general_repo'.format(os.path.sep)
     return general_repo_dir
--- tests/test_encoding_validators/test_are_sources_in_utf.py
+++ tests/test_encoding_validators/test_are_sources_in_utf.py
@@ -1,12 +1,12 @@
 from fiasko_bro.pre_validation_checks import are_sources_in_utf
 
 
-def test_are_sources_in_utf_fail(encoding_repo):
-    output = are_sources_in_utf(encoding_repo)
+def test_are_sources_in_utf_fail(encoding_repo_path):
+    output = are_sources_in_utf(encoding_repo_path)
     assert isinstance(output, tuple)
     assert output[0] == 'sources_not_utf_8'
 
 
-def test_are_sources_in_utf_ok(general_repo):
-    output = are_sources_in_utf(general_repo)
+def test_are_sources_in_utf_ok(general_repo_path):
+    output = are_sources_in_utf(general_repo_path)
     assert output is None
