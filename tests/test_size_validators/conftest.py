1f6eb733143fd5406b26161dddbdf260b03b8f4f
Artem Chernyaev <askanio234@gmail.com>
2018-02-17 14:37:37 +0300

    Rename test fixtures for repo size tests

--- tests/test_size_validators/conftest.py
+++ tests/test_size_validators/conftest.py
@@ -4,12 +4,12 @@ import pytest
 
 
 @pytest.fixture(scope="module")
-def general_repo_origin():
+def general_repo_origin_path():
     general_repo_origin_dir = 'test_fixtures{}general_repo_origin'.format(os.path.sep)
     return general_repo_origin_dir
 
 
 @pytest.fixture(scope="module")
-def general_repo():
+def general_repo_path():
     general_repo_dir = 'test_fixtures{}general_repo'.format(os.path.sep)
     return general_repo_dir
--- tests/test_size_validators/test_are_repos_to_large.py
+++ tests/test_size_validators/test_are_repos_to_large.py
@@ -1,27 +1,27 @@
 from fiasko_bro.pre_validation_checks import are_repos_too_large
 
 
-def test_repo_size_fail_single(general_repo):
+def test_repo_size_fail_single(general_repo_path):
     max_py_files_count = 1
-    output = are_repos_too_large(general_repo, max_py_files_count)
+    output = are_repos_too_large(general_repo_path, max_py_files_count)
     assert isinstance(output, tuple)
     assert output[0] == 'Repo is too large'
 
 
-def test_repo_size_fail_double(general_repo, general_repo_origin):
+def test_repo_size_fail_double(general_repo_path, general_repo_origin_path):
     max_py_files_count = 1
-    output = are_repos_too_large(general_repo, max_py_files_count, general_repo_origin)
+    output = are_repos_too_large(general_repo_path, max_py_files_count, general_repo_origin_path)
     assert isinstance(output, tuple)
     assert output[0] == 'Repo is too large'
 
 
-def test_repo_size_ok_single(general_repo):
+def test_repo_size_ok_single(general_repo_path):
     max_py_files_count = 1000
-    output = are_repos_too_large(general_repo, max_py_files_count)
+    output = are_repos_too_large(general_repo_path, max_py_files_count)
     assert output is None
 
 
-def test_repo_size_ok_double(general_repo, general_repo_origin):
+def test_repo_size_ok_double(general_repo_path, general_repo_origin_path):
     max_py_files_count = 1000
-    output = are_repos_too_large(general_repo, max_py_files_count, general_repo_origin)
+    output = are_repos_too_large(general_repo_path, max_py_files_count, general_repo_origin_path)
     assert output is None
