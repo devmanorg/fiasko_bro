d160d31f9221e5ff06db66c7a6b17456841d6b67
melevir <melevir@gmail.com>
2018-02-20 09:28:54 +0300

    refactor are_sources_in_utf validator

--- fiasko_bro/file_helpers.py
+++ fiasko_bro/file_helpers.py
@@ -22,3 +22,14 @@ def get_line_offsets(file_content):
     for line in file_content.split('\n'):
         lines_offsets.append(len(line) - len(line.lstrip(' ')))
     return lines_offsets
+
+
+def is_in_utf8(name):
+    if not (name.endswith('.py') or name.endswith('.txt')):
+        return True
+    try:
+        with open((name), mode='r', encoding='utf-8') as file_handle:
+            file_handle.read()
+    except UnicodeDecodeError:
+        return False
+    return True
--- fiasko_bro/pre_validation_checks/encoding.py
+++ fiasko_bro/pre_validation_checks/encoding.py
@@ -1,12 +1,10 @@
 import os
 
+from ..file_helpers import is_in_utf8
+
 
 def are_sources_in_utf(path_to_repo, *args, **kwargs):
     for root, dirs, filenames in os.walk(path_to_repo):
         for name in filenames:
-            if name.endswith('.py') or name.endswith('.txt'):
-                try:
-                    with open((os.path.join(root, name)), mode='r', encoding='utf-8') as file_handle:
-                        file_handle.read()
-                except UnicodeDecodeError:
-                    return 'sources_not_utf_8', None
+            if not is_in_utf8(os.path.join(root, name)):
+                return 'sources_not_utf_8', None
