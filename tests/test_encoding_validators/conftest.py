02b0117965096d4128a319f6e6deffe97d79c56e
Artem Chernyaev <askanio234@gmail.com>
2018-02-23 13:57:37 +0300

    Add tests for has_no_bom validator, progress towards Issue #6

--- tests/test_encoding_validators/conftest.py
+++ tests/test_encoding_validators/conftest.py
@@ -1,6 +1,7 @@
 import os.path
 import pytest
-
+import git
+from fiasko_bro import LocalRepositoryInfo
 
 
 @pytest.fixture(scope="module")
@@ -13,3 +14,17 @@ def encoding_repo_path():
 def general_repo_path():
     general_repo_dir = 'test_fixtures{}general_repo'.format(os.path.sep)
     return general_repo_dir
+
+
+@pytest.fixture(scope="module")
+def test_repo_with_bom():
+    test_repo_dir = 'test_fixtures{0}encoding_repo{0}utf8_with_bom'.format(os.path.sep)
+    git.Repo.init(test_repo_dir)
+    return LocalRepositoryInfo(test_repo_dir)
+
+
+@pytest.fixture(scope="module")
+def test_repo_without_bom():
+    test_repo_dir = 'test_fixtures{}general_repo'.format(os.path.sep)
+    git.Repo.init(test_repo_dir)
+    return LocalRepositoryInfo(test_repo_dir)
