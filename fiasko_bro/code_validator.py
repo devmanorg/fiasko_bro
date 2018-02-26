b01c1c2ca566ee0c9da97583a2c9242b35ef9b63
Artem Chernyaev <askanio234@gmail.com>
2018-02-26 20:53:27 +0300

    Remove get_syntax_errors_in_repo function

--- fiasko_bro/code_validator.py
+++ fiasko_bro/code_validator.py
@@ -146,11 +146,6 @@ class CodeValidator:
             return errors
         return errors
 
-    def get_syntax_errors_in_repo(self, repo):
-        syntax_errors = validators.has_no_syntax_errors(repo)
-        if syntax_errors:
-            return [syntax_errors]
-
     def validate(self, repo_path, original_repo_path=None, **kwargs):
         self.validator_arguments.update(kwargs)
         self.validator_arguments['path_to_repo'] = repo_path
@@ -163,9 +158,9 @@ class CodeValidator:
         self.validator_arguments['solution_repo'] = LocalRepositoryInfo(repo_path)
         if original_repo_path:
             self.validator_arguments['original_repo'] = LocalRepositoryInfo(original_repo_path)
-        syntax_errors = self.get_syntax_errors_in_repo(
+        syntax_errors = validators.has_no_syntax_errors(
             self.validator_arguments['solution_repo']
         )
         if syntax_errors:
-            return syntax_errors
+            return [syntax_errors]
         return self.run_validator_group(self.error_validator_groups, add_warnings=True)
