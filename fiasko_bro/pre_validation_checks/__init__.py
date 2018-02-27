5890c9832bd5dc7cbaad51aba734d70ce65179a6
Artem Chernyaev <askanio234@gmail.com>
2018-02-27 23:09:02 +0300

    Add new prevalidator - BOM Closes #86

--- fiasko_bro/pre_validation_checks/__init__.py
+++ fiasko_bro/pre_validation_checks/__init__.py
@@ -1,2 +1,3 @@
 from .encoding import *
 from .repo_size import *
+from .bom import *
---
+++ fiasko_bro/pre_validation_checks/bom.py
@@ -0,0 +1,16 @@
+import os
+import codecs
+from fiasko_bro.config import VALIDATOR_SETTINGS
+
+
+def has_no_bom(path_to_repo, *args, **kwargs):
+    for root, dirs, filenames in os.walk(path_to_repo):
+        dirs[:] = [
+            d for d in dirs
+            if d not in VALIDATOR_SETTINGS['directories_to_skip']
+        ]
+        for name in filenames:
+            with open(os.path.join(root, name), 'rb') as file_handle:
+                file_content = file_handle.read()
+                if file_content.startswith(codecs.BOM_UTF8):
+                    return 'has_bom', name
