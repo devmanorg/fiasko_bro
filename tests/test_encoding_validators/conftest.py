2893b7582d21e7c1d75103564f1c5cebe65b5775
Artem Chernyaev <askanio234@gmail.com>
2018-02-27 23:19:01 +0300

    Fix tests for new prevalidator

--- tests/test_encoding_validators/conftest.py
+++ tests/test_encoding_validators/conftest.py
@@ -1,7 +1,5 @@
 import os.path
 import pytest
-import git
-from fiasko_bro import LocalRepositoryInfo
 
 
 @pytest.fixture(scope="module")
@@ -17,14 +15,12 @@ def general_repo_path():
 
 
 @pytest.fixture(scope="module")
-def test_repo_with_bom():
+def test_repo_with_bom_path():
     test_repo_dir = 'test_fixtures{0}encoding_repo{0}utf8_with_bom'.format(os.path.sep)
-    git.Repo.init(test_repo_dir)
-    return LocalRepositoryInfo(test_repo_dir)
+    return test_repo_dir
 
 
 @pytest.fixture(scope="module")
-def test_repo_without_bom():
+def test_repo_without_bom_path():
     test_repo_dir = 'test_fixtures{}general_repo'.format(os.path.sep)
-    git.Repo.init(test_repo_dir)
-    return LocalRepositoryInfo(test_repo_dir)
+    return test_repo_dir
--- tests/test_encoding_validators/test_has_no_BOM.py
+++ tests/test_encoding_validators/test_has_no_BOM.py
@@ -1,12 +1,12 @@
-from fiasko_bro.validators import has_no_bom
+from fiasko_bro.pre_validation_checks import has_no_bom
 
 
-def test_has_no_bom_fail(test_repo_with_bom):
-    output = has_no_bom(test_repo_with_bom)
+def test_has_no_bom_fail(test_repo_with_bom_path):
+    output = has_no_bom(test_repo_with_bom_path)
     assert isinstance(output, tuple)
     assert output[0] == 'has_bom'
 
 
-def test_has_no_bom_ok(test_repo_without_bom):
-    output = has_no_bom(test_repo_without_bom)
+def test_has_no_bom_ok(test_repo_without_bom_path):
+    output = has_no_bom(test_repo_without_bom_path)
     assert output is None
