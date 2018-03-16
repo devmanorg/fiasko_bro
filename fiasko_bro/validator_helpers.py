fc02cc8fcbcc8eadcf8407a98e93b7e948bfd997
Artem Chernyaev <askanio234@gmail.com>
2018-03-16 22:02:49 +0300

    Enable tokenized_validator to accept multiple tokens Close #5

--- fiasko_bro/validator_helpers.py
+++ fiasko_bro/validator_helpers.py
@@ -1,11 +1,13 @@
 from functools import wraps
 
 
-def tokenized_validator(token):
+def tokenized_validator(list_of_tokens):
     def validator_decorator(func):
         @wraps(func)
         def func_wrapper(*args, **kwargs):
-            if token == kwargs.get('validator_token'):
-                return func(*args, **kwargs)
+            repo_tokens = kwargs.get('validator_token')
+            if repo_tokens:
+                if [token for token in list_of_tokens if token in repo_tokens]:
+                    return func(*args, **kwargs)
         return func_wrapper
     return validator_decorator
