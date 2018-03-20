ad13a5d405e671c0c3302d06eda66cb675b5e1ca
Artem Chernyaev <askanio234@gmail.com>
2018-03-20 22:47:40 +0300

    Reworked tests to match tokenized validator interface

--- tests/test_validation_interface/test_tokenized_validator_work.py
+++ tests/test_validation_interface/test_tokenized_validator_work.py
@@ -2,25 +2,50 @@ import os
 import pytest
 
 from .utils import initialize_repo
-from fiasko_bro.validator_helpers import tokenized_validator
+from fiasko_bro.validator_helpers import (
+    tokenized_validator_run_if_any,
+    tokenized_validator_run_if_all,
+    tokenized_validator_run_if
+)
 from fiasko_bro import CodeValidator
 
 
-@tokenized_validator(['always_give_error'])
-def get_error_always_validator(solution_repo, *args, **kwargs):
-    return 'always error',
+MESSAGE = 'validator runs'
 
 
-@tokenized_validator(['token_a', 'token_b'])
-def get_error_always_validator_with_two_tokens(solution_repo, *args, **kwargs):
-    return 'always error with two tokens',
+@pytest.fixture(scope='module')
+def get_validator_with_single_token(token):
+    @tokenized_validator_run_if(token)
+    def tokenized_validator_with_single_token(solution_repo, *args, **kwargs):
+        return MESSAGE,
+    return tokenized_validator_with_single_token
+
+
+@pytest.fixture(scope='module')
+def get_validator_with_two_optional_tokens(iterable):
+    @tokenized_validator_run_if_any(iterable)
+    def tokenized_validator_with_two_optional_tokens(solution_repo, *args, **kwargs):
+        return MESSAGE,
+    return tokenized_validator_with_two_optional_tokens
+
+
+@pytest.fixture(scope='module')
+def get_validator_with_two_mandatory_tokens(iterable):
+    @tokenized_validator_run_if_all(iterable)
+    def tokenized_validator_with_two_mandatory_tokens(solution_repo, *args, **kwargs):
+        return MESSAGE,
+    return tokenized_validator_with_two_mandatory_tokens
 
 
 @pytest.fixture(scope='module')
 def code_validator():
     code_validator = CodeValidator()
-    code_validator.error_validator_groups['commits'].append(get_error_always_validator)
-    code_validator.error_validator_groups['commits'].append(get_error_always_validator_with_two_tokens)
+    validator_with_single_token = get_validator_with_single_token('sqlalchemy')
+    validator_with_two_optional_tokens = get_validator_with_two_optional_tokens({'minmax', 'maximize'})
+    validator_with_two_mandatory_tokens = get_validator_with_two_mandatory_tokens(['django', 'twisted'])
+    code_validator.error_validator_groups['commits'].append(validator_with_single_token)
+    code_validator.error_validator_groups['commits'].append(validator_with_two_optional_tokens)
+    code_validator.error_validator_groups['commits'].append(validator_with_two_mandatory_tokens)
     return code_validator
 
 
@@ -31,21 +56,31 @@ def origin_repo():
     return repo_path
 
 
-def test_tokenized_validator_ok(origin_repo, code_validator):
-    output = code_validator.validate(origin_repo, validator_token='always_give_error')
-    assert ('always error',) in output
+def test_tokenized_validator_with_single_token_ok(origin_repo, code_validator):
+    output = code_validator.validate(origin_repo, validator_token='sqlalchemy')
+    assert (MESSAGE,) in output
 
 
-def test_tokenized_validator_fail(origin_repo, code_validator):
+def test_tokenized_validator_with_single_token_fail(origin_repo, code_validator):
     output = code_validator.validate(origin_repo, validator_token=None)
-    assert ('always error',) not in output
+    assert (MESSAGE,) not in output
 
 
-def test_tokenized_validator_with_mult_tokens_ok(origin_repo, code_validator):
-    output = code_validator.validate(origin_repo, validator_token=['other_token', 'token_b'])
-    assert ('always error with two tokens',) in output
+def test_tokenized_validator_with_optional_tokens_ok(origin_repo, code_validator):
+    output = code_validator.validate(origin_repo, validator_tokens=['maximize', 'minmax', 'sql'])
+    assert (MESSAGE,) in output
 
 
-def test_tokenized_validator_with_mult_tokens_fail(origin_repo, code_validator):
-    output = code_validator.validate(origin_repo, validator_token=['other_token', 'token_c'])
-    assert ('always error with two tokens',) not in output
+def test_tokenized_validator_with_optional_tokens_fail(origin_repo, code_validator):
+    output = code_validator.validate(origin_repo, validator_tokens=['sql', 'concurrency'])
+    assert (MESSAGE,) not in output
+
+
+def test_validator_with_mandatory_tokens_ok(origin_repo, code_validator):
+    output = code_validator.validate(origin_repo, validator_tokens=('twisted', 'django'))
+    assert (MESSAGE,) in output
+
+
+def test_validator_with_mandatory_tokens_fail(origin_repo, code_validator):
+    output = code_validator.validate(origin_repo, validator_tokens={'django', 'tornado'})
+    assert (MESSAGE,) not in output
