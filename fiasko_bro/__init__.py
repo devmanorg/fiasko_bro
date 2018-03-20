51034673303c6f7e9ac8e8c2ab07f31ef6b6d63d
Artem Chernyaev <askanio234@gmail.com>
2018-03-20 22:22:26 +0300

    Fix style for long import line

--- fiasko_bro/__init__.py
+++ fiasko_bro/__init__.py
@@ -1,5 +1,7 @@
 from .code_validator import CodeValidator, validate_repo
-from .validator_helpers import (tokenized_validator_run_if_any,
-                                tokenized_validator_run_if_all,
-                                tokenized_validator_run_if)
+from .validator_helpers import (
+    tokenized_validator_run_if_any,
+    tokenized_validator_run_if_all,
+    tokenized_validator_run_if
+)
 from .repository_info import LocalRepositoryInfo
