c3f4b3516bf70f29d9964a4a61ad2501f11c1e81
Artem Chernyaev <askanio234@gmail.com>
2018-03-20 21:13:01 +0300

    Split tokenized validator into three: if_any, if_all and if

--- fiasko_bro/__init__.py
+++ fiasko_bro/__init__.py
@@ -1,3 +1,3 @@
 from .code_validator import CodeValidator, validate_repo
-from .validator_helpers import tokenized_validator
+from .validator_helpers import tokenized_validator_if_any, tokenized_validator_if_all, tokenized_validator_if
 from .repository_info import LocalRepositoryInfo
--- fiasko_bro/validator_helpers.py
+++ fiasko_bro/validator_helpers.py
@@ -1,13 +1,36 @@
 from functools import wraps
 
 
-def tokenized_validator(list_of_tokens):
+def _general_tokenized_validator(tokens, check_method):
     def validator_decorator(func):
         @wraps(func)
         def func_wrapper(*args, **kwargs):
             repo_tokens = kwargs.get('validator_token')
+            if repo_tokens is None:
+                repo_tokens = kwargs.get('validator_tokens')
             if repo_tokens:
-                if [token for token in list_of_tokens if token in repo_tokens]:
+                if check_method(tokens, repo_tokens):
                     return func(*args, **kwargs)
         return func_wrapper
     return validator_decorator
+
+
+def tokenized_validator_if_any(tokens):
+    return _general_tokenized_validator(tokens, if_any)
+
+
+def tokenized_validator_if_all(tokens):
+    return _general_tokenized_validator(tokens, if_all)
+
+
+def tokenized_validator_if(token):
+    return _general_tokenized_validator(token, if_all)
+
+
+def if_any(tokens, repo_tokens):
+    return any(token for token in tokens if token in repo_tokens)
+
+
+def if_all(tokens, repo_tokens):
+    print(tokens, repo_tokens)
+    return set(tokens) == set(repo_tokens)
