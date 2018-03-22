356723c6075578e1a974118ec18802b30d619d17
Artem Chernyaev <askanio234@gmail.com>
2018-03-22 22:33:08 +0300

    Rafactor _general_tokenized_validator function

--- fiasko_bro/tokenized_validators/tokenized_validators.py
+++ fiasko_bro/tokenized_validators/tokenized_validators.py
@@ -5,12 +5,9 @@ def _general_tokenized_validator(tokens, check_method):
     def validator_decorator(func):
         @wraps(func)
         def func_wrapper(*args, **kwargs):
-            repo_tokens = kwargs.get('validator_token')
-            if repo_tokens is None:
-                repo_tokens = kwargs.get('validator_tokens')
-            if repo_tokens:
-                if check_method(tokens, repo_tokens):
-                    return func(*args, **kwargs)
+            repo_tokens = kwargs.get('validator_token') or kwargs.get('validator_tokens')
+            if repo_tokens and check_method(tokens, repo_tokens):
+                return func(*args, **kwargs)
         return func_wrapper
     return validator_decorator
 
