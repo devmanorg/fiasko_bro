ac466fae202ff5bc8f1221b1e71ff6d73ef7c9e7
Artem Chernyaev <askanio234@gmail.com>
2018-03-30 20:42:23 +0300

    Remove unnecessary imports in fiasko_bro __init__

--- fiasko_bro/__init__.py
+++ fiasko_bro/__init__.py
@@ -1,3 +1,2 @@
 from .code_validator import CodeValidator, validate_repo
-from .tokenized_validators import run_if, run_if_any, run_if_all, run_if_tokens_satisfy_condition
 from .repository_info import LocalRepositoryInfo
