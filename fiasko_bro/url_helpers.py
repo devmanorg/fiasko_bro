ee2d6a17e8619a0345c2f257710153b29e5f6d47
Paul Vergeev <paulvergeev@gmail.com>
2018-04-07 17:58:56 +0300

    Revert "Remove the old validator API"
    This reverts commit f4dea0b9577fc4d5080a9d003024fd7344767c0e.

--- fiasko_bro/url_helpers.py
+++ fiasko_bro/url_helpers.py
@@ -9,3 +9,7 @@ def is_url_with_params(string):
         if '=' not in key_value_pair:
             return False
     return True
+
+
+def get_filename_from_path(path):
+    return path[path.rfind(os.path.sep) + 1:]
