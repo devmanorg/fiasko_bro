d178c81d140cb9e9305ba8c1443419821a8efed0
Paul Vergeev <paulvergeev@gmail.com>
2018-04-20 10:53:48 +0300

    Add get_error_slugs for easier slug existence check

--- fiasko_bro/__init__.py
+++ fiasko_bro/__init__.py
@@ -1,3 +1,3 @@
-from .code_validator import validate
+from .code_validator import validate, get_error_slugs
 from .validator_helpers import tokenized_validator
 from .repository_info import ProjectFolder
