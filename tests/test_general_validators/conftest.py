978a27b764513360f2247675f8df227a0db7f47b
Paul Vergeev <paulvergeev@gmail.com>
2018-04-28 10:08:40 +0300

    Turn general repo into an actual git repo
    This will be needed for some of the validators in the general group

--- tests/test_general_validators/conftest.py
+++ tests/test_general_validators/conftest.py
@@ -2,6 +2,7 @@ import os.path
 
 import pytest
 
+from tests.utils import initialize_repo, remove_repo
 from fiasko_bro import defaults
 from fiasko_bro.repository_info import ProjectFolder
 
@@ -9,12 +10,16 @@ from fiasko_bro.repository_info import ProjectFolder
 @pytest.fixture(scope="module")
 def test_repo():
     test_repo_dir = 'test_fixtures{}general_repo'.format(os.path.sep)
+    initialize_repo(test_repo_dir)
     directories_to_skip = defaults.VALIDATION_PARAMETERS['directories_to_skip']
-    return ProjectFolder(test_repo_dir, directories_to_skip)
+    yield ProjectFolder(test_repo_dir, directories_to_skip)
+    remove_repo(test_repo_dir)
 
 
 @pytest.fixture(scope="module")
 def origin_repo():
     origin_repo_dir = 'test_fixtures{}general_repo_origin'.format(os.path.sep)
+    initialize_repo(origin_repo_dir)
     directories_to_skip = defaults.VALIDATION_PARAMETERS['directories_to_skip']
-    return ProjectFolder(origin_repo_dir, directories_to_skip)
+    yield ProjectFolder(origin_repo_dir, directories_to_skip)
+    remove_repo(origin_repo_dir)
--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,14 @@
+import shutil
+
 import git
+
 
 def initialize_repo(repo_path):
     repo = git.Repo.init(repo_path)
     repo.index.add(['.'])
     repo.index.commit('Initial commit')
+
+
+def remove_repo(repo_path):
+    git_folder_path = '{}/.git'.format(repo_path)
+    shutil.rmtree(git_folder_path)
